bdf4bc82c05a1f09368af20ca6b079d7591db8c6
Jiali Wang <wjlbmw@gmail.com>
2019-04-04 10:35:15 -0500

    Landing page - post route to submit email

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ let landing = require('../controllers/landing');
 /* GET home page. */
 // router.get('/', index.index);
 router.get('/', landing.get_landing);
+router.get('/', landing.submit_lead);
 
 module.exports = router;
