8d4ee42eddf14edcedd152d04c38eb1d971f8ac1
Jiali Wang <wjlbmw@gmail.com>
2019-04-04 16:38:46 -0500

    Implement /leads route
    Show a collection of leads upon submission

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ let landing = require('../controllers/landing');
 // router.get('/', index.index);
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
+router.get('/leads', landing.show_leads);
 
 module.exports = router;
